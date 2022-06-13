dc060b0a1c88e3131433513d4e6fb68bf0d27e58
h662 <h662hong@gmail.com>
2022-06-13 21:43:25 +0900

    2. NFT Metadata

--- contracts/MintNFT.sol
+++ contracts/MintNFT.sol
@@ -5,11 +5,21 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MintNFT is ERC721Enumerable {
+    mapping(uint => string) public metadataURIs;
+    
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
     
     function mintNFT() public {
         uint tokenId = totalSupply() + 1;
 
         _mint(msg.sender, tokenId);
+    }
+
+    function setTokenURI(uint _tokenId, string memory _metadataURI) public {
+        metadataURIs[_tokenId] = _metadataURI;
+    }
+
+    function tokenURI(uint _tokenId) override public view returns (string memory) {
+        return metadataURIs[_tokenId];
     }
 }
