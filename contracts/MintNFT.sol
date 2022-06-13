c21b1bea3470b720651479343c6e22e853d740a9
h662 <h662hong@gmail.com>
2022-06-14 08:38:41 +0900

    3. Ownable

--- contracts/MintNFT.sol
+++ contracts/MintNFT.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintNFT is ERC721Enumerable {
+contract MintNFT is ERC721Enumerable, Ownable {
     mapping(uint => string) public metadataURIs;
     
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
@@ -15,7 +16,7 @@ contract MintNFT is ERC721Enumerable {
         _mint(msg.sender, tokenId);
     }
 
-    function setTokenURI(uint _tokenId, string memory _metadataURI) public {
+    function setTokenURI(uint _tokenId, string memory _metadataURI) public onlyOwner {
         metadataURIs[_tokenId] = _metadataURI;
     }
 
